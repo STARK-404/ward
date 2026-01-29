f39c50e1961cf2353fd8b624f5d375aba8b4e265
STARK-404 <134937814+STARK-404@users.noreply.github.com>
2026-01-29 19:31:13 +0530

    Use hardcoded MONGO_URI for database connection

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const MONGO_URI = "mongodb+srv://lalkrishnameet_db_user:J3jb9PcHC2cxYs04@cluster0.gveaoha.mongodb.net/?appName=Cluster0";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
